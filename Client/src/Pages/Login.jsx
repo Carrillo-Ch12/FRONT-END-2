7b635de30b927923c918365e8000cdb8a49b2d32
martincarrillo <martin.carrillo@alumnos.uv.cl>
2023-10-08 18:56:20 -0300

    Login funcional

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,34 +1,78 @@
-import React, { Component, useState } from 'react'
-import '../Components/Login.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Component } from 'react';
+import '../Components/Login.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
-export default function Login() {
-  const[values,setvalues]= useState({
-    Usuaio:'',
-    Password:''
-  })
-  return (
+const baseUrl = "http://localhost:4001/login";
 
-    <div className="Principal">
-      <div className="form-group">
-        <h2>Login</h2>
-        <form action="" >
-        <br />
-        <label>Usuario: </label>
-        <br />
-        <input type="text"className="form-control"name="username"/>
-        <br />
-        <label>Contraseña: </label>
-        <br />
-        <input type="password"className="form-control"name="password"/>
-        <br />
-        <button type='submit'className="btn btn-primary" >Iniciar Sesión</button>
-    
-        <a href="/Registro"><button className="btn btn-primary" >Registrarme</button></a>
-        </form>
+export default class Login extends Component {
+  state = {
+    form: {
+      Nombre_usuario: '',
+      Contraseña: '',
+    },
+  };
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      form: {
+        ...this.state.form,
+        [name]: value,
+      },
+    });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(baseUrl, this.state.form);
+      console.log(response);
+      if (response.status=200) {
+        window.location.href = "/Biblioteca";
+      } else {
+        console.log("Credenciales incorrectas");
+      }
+    } catch (error) {
+      console.log("Error en la solicitud:", error);
+    }
+  };
+
+  render() {
+    return (
+      <div className="fondo">
+        <div className="Principal">
+          <div className="Secundario">
+            <div className="form-group">
+              <form onSubmit={this.handleSubmit}>
+                <label>Usuario:</label>
+                <br />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="Nombre_usuario"
+                  value={this.state.form.Nombre_usuario}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <label>Contraseña:</label>
+                <br />
+                <input
+                  type="password"
+                  className="form-control"
+                  name="Contraseña"
+                  value={this.state.form.Contraseña}
+                  onChange={this.handleChange}
+                />
+                <br />
+                <button className="btn btn-primary" type="submit">
+                  Iniciar sesión
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  
     );
-  
+  }
 }
