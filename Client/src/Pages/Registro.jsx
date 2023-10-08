b70c77a6d01729c20dddeed9a25b4458ab0e31d2
martincarrillo <martin.carrillo@alumnos.uv.cl>
2023-10-08 16:15:54 -0300

    Validation

--- Client/src/Pages/Registro.jsx
+++ Client/src/Pages/Registro.jsx
@@ -1,31 +1,28 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import '../Components/Login.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default function Login() {
-  const[values,setvalues]= useState({
-    Usuaio:'',
-    Password:''
-  })
+
+
+export default function Registro() {
   return (
 
     <div className="Principal">
       <div className="form-group">
-        <h2>Login</h2>
-        <form action="" >
+        <h2>Registro</h2>
         <br />
         <label>Usuario: </label>
         <br />
         <input type="text"className="form-control"name="username"/>
+        <label>Correo: </label>
         <br />
+        <input type="text" className="form-control"name="username"/>
         <label>Contraseña: </label>
         <br />
         <input type="password"className="form-control"name="password"/>
         <br />
-        <button type='submit'className="btn btn-primary" >Iniciar Sesión</button>
-    
         <a href="/Registro"><button className="btn btn-primary" >Registrarme</button></a>
-        </form>
+        
       </div>
     </div>
   
--- Client/src/Routes/app.routes.jsx
+++ Client/src/Routes/app.routes.jsx
@@ -3,12 +3,14 @@ import Login from "../Pages/Login.jsx"
 import Bliblioteca from "../Pages/Biblioteca.jsx"
 import Error from "../Pages/Error.jsx"
 import Perfil from "../Pages/Perfil.jsx"
+import Registro from "../Pages/Registro.jsx"
 export const AppRouter = props => {
   return (
     <Routes>
         <Route path='/' element= {<Login/>}/>
         <Route exact path="/Biblioteca" element= {<Bliblioteca/>}/>
         <Route exact path="Perfil" element= {<Perfil/>}/>
+        <Route exact path="Registro" element= {<Registro/>}/>
         <Route path="/*" element= {<Error/>}/>
 
     </Routes>
