652bb47c1e50a3d5931fbd43b2709436aa030060
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2023-10-15 00:19:21 -0300

    Biblioteca.jsx

--- src/pages/Paginas/Biblioteca.jsx
+++ src/pages/Paginas/Biblioteca.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PageContainer from "../../components/container/PageContainer";
+import axios from 'axios';
+import { Link } from "react-router-dom";
 
+const baseUrl = "http://localhost:4001/biblioteca";
 
-function Biblioteca() {
+export const Biblioteca = () => {
+  const [videojuegos, setVideojuegos] = useState([]); // Definir el estado inicial como un arreglo vacío
+
+  const getVideojuegos = async () => {
+    try {
+      const response = await axios.get(baseUrl);
+      setVideojuegos(response.data); // Actualizar el estado con los datos obtenidos
+    } catch (error) {
+      console.error("Error al obtener los videojuegos", error);
+    }
+  };
+
+  useEffect(() => {
+    getVideojuegos();
+  }, []);
+
   return (
-    <PageContainer title="Biblioteca" d>
-            <h1>hola</h1>
-    </PageContainer>
-  );
-}
+    <>
+      <p>Se encontraron {videojuegos.length} resultados</p>
+      <div>className = videojuegos.list</div>
+        {videojuegos.map(videojuego => (
+          <Link>
+            <div className="videoJuegos-list">
+              <img src={videojuego.img} alt={videojuego.titulo}/>
+            </div>
+          </Link>
+        
+        ))}
+    </>
+  )
+};
 
 export default Biblioteca;
+
