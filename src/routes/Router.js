d4a9823aa8d692110ae9478e1bbcfa7abf7a828f
Carrillo-Ch <martin.carrillo@alumnos.uv.cl>
2023-10-17 11:15:33 -0300

    Register front

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -15,6 +15,7 @@ const Error = Loadable(lazy(() => import("../pages/Error/404")));
 
 const Biblioteca = Loadable(lazy(() => import("../pages/Paginas/Biblioteca")));
 const Login = Loadable(lazy(() => import("../pages/Paginas/Login")));
+const Registro = Loadable(lazy(() => import("../pages/Paginas/Registro")));
 
 
 const Router = [
@@ -26,6 +27,7 @@ const Router = [
       { path: "*", element: <Navigate to="/404" /> },
       { path: "404", element: <Error /> },
       { path: "Biblioteca", element: <Biblioteca /> },
+      { path: "Registro", element: <Registro /> },
     ],
   },
 ];
