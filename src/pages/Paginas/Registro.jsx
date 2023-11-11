09c5786c7ade48b78be0fd8d4eae3b74f2f50e3e
Carrillo-Ch <martin.carrillo@alumnos.uv.cl>
2023-11-11 13:46:17 -0300

    de clase a funcion

--- src/pages/Paginas/Registro.jsx
+++ src/pages/Paginas/Registro.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import './components/Login.css';
 
-const baseUrl2 = "http://localhost:4001/api/Registro"; 
+const baseUrl2 = "http://localhost:4001/api/Registro";
 
-export default class Registro extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Nombre_usuario: '',
-      Correo: '',
-      Contraseña: '',
-      Foto_de_perfil: '', 
-    };
-  }
+const Registro = () => {
+  const [form, setForm] = useState({
+    Nombre_usuario: '',
+    Correo: '',
+    Contraseña: '',
+    Foto_de_perfil: '',
+  });
 
-  handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(baseUrl2, this.state); 
+      const response = await axios.post(baseUrl2, form);
       console.log(response);
-      if (response.status === 200) { 
+      if (response.status === 200) {
         window.location.href = "/";
       } else {
         console.log("Credenciales incorrectas");
@@ -31,64 +28,66 @@ export default class Registro extends Component {
     }
   };
 
-  render() {
-    return (
-      <div>
-        <div className="PrincipalRegistro">
-          <div className="Secundario">
-            <div className="form-group">
-              <form onSubmit={this.handleSubmit}>
-                <label>Nombre de usuario:</label>
-                <br />
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Nombre_usuario"
-                  value={this.state.Nombre_usuario}
-                  onChange={(e) =>
-                    this.setState({ Nombre_usuario: e.target.value })
-                  }
-                />
-                <label>Correo:</label>
-                <br />
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Correo"
-                  value={this.state.Correo}
-                  onChange={(e) => this.setState({ Correo: e.target.value })}
-                />
-                <label>Contraseña:</label>
-                <br />
-                <input
-                  type="password"
-                  className="form-control"
-                  name="Contraseña"
-                  value={this.state.Contraseña}
-                  onChange={(e) =>
-                    this.setState({ Contraseña: e.target.value })
-                  }
-                />
-                <label>Foto de perfil (url):</label>
-                <br />
-                <input
-                  type="text"
-                  className="form-control"
-                  name="Foto_de_perfil" 
-                  value={this.state.Foto_de_perfil} 
-                  onChange={(e) =>
-                    this.setState({ Foto_de_perfil: e.target.value }) 
-                  }
-                />
-                <br />
-                <button className="btn btn-primary" type="submit">
-                  Registrarme
-                </button>
-              </form>
-            </div>
+  return (
+    <div>
+      <div className="PrincipalRegistro">
+        <div className="Secundario">
+          <div className="form-group">
+            <form onSubmit={handleSubmit}>
+              <label>Nombre de usuario:</label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="Nombre_usuario"
+                value={form.Nombre_usuario}
+                onChange={(e) =>
+                  setForm({ ...form, Nombre_usuario: e.target.value })
+                }
+              />
+              <label>Correo:</label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="Correo"
+                value={form.Correo}
+                onChange={(e) =>
+                  setForm({ ...form, Correo: e.target.value })
+                }
+              />
+              <label>Contraseña:</label>
+              <br />
+              <input
+                type="password"
+                className="form-control"
+                name="Contraseña"
+                value={form.Contraseña}
+                onChange={(e) =>
+                  setForm({ ...form, Contraseña: e.target.value })
+                }
+              />
+              <label>Foto de perfil (url):</label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="Foto_de_perfil"
+                value={form.Foto_de_perfil}
+                onChange={(e) =>
+                  setForm({ ...form, Foto_de_perfil: e.target.value })
+                }
+              />
+              <br />
+              <button className="btn btn-primary" type="submit">
+                Registrarme
+              </button>
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Registro;
